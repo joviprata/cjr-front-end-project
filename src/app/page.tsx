9f04d9856c21e827ff3ee200e1ee2bc171a3c402
Jovi Prata <115483518+joviprata@users.noreply.github.com>
2024-05-25 01:01:29 -0300

    Importar página de Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import FeedDeslogado from "./FeedDeslogado";
 import FeedLogado from "./FeedLogado";
 import PerfilLogado from "./PerfilLogado";
 import PerfilDeslogado from "./PerfilDeslogado";
+import LoginPage from "./Login";
+
 export default function Home() {
   return (
     <PerfilDeslogado />
